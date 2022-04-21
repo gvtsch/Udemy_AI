ff3de9ab059ec2ff3cf9c2b3792c6fc4a59f928a
Christoph Kempkes <Kempkes.Christoph@gmail.com>
2022-04-21 06:59:20 +0200

    Main

---
+++ Chapter10_DeepQNetworks/cartPoleDqn.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Activation, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+class DQN(tf.keras.Model):
+    def __init__():
+        pass
+
+    def call(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def update_model(self):
+        pass
+
+    def load_model(self):
+        pass
+
+    def save_model(self):
+        pass
